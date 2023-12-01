1bcfac2fee13f1ef34d9b9d2afcdc730a632b4ee
Daniel Fitzpatrick <crinklywrappr@pm.me>
2023-12-01 12:01:39 -0600

    creation + 2023 day one

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to crinklywrappr/aoc
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/crinklywrappr/aoc/2023/day1.clj
@@ -0,0 +1,26 @@
+(ns crinklywrappr.aoc.2023.day1
+  (:require [clojure.java.io :as io]))
+
+(def file (io/resource "2023/day1.txt"))
+
+(defn part1 []
+  (with-open [rdr (io/reader file)]
+    (-> (map (comp parse-long
+                (partial apply str)
+                (juxt first last)
+                (partial re-seq #"\d")))
+        (transduce + (line-seq rdr)))))
+
+(def rgx #"(?=(one|two|three|four|five|six|seven|eight|nine|\d))")
+(def xf {"one" 1 "two" 2 "three" 3 "four" 4 "five" 5 "six" 6 "seven" 7 "eight" 8 "nine" 9
+         "1" 1 "2" 2 "3" 3 "4" 4 "5" 5 "6" 6 "7" 7 "8" 8 "9" 9})
+
+(defn part2 []
+  (with-open [rdr (io/reader file)]
+    (-> (map (comp (partial apply +)
+                (juxt (comp (partial * 10) xf first)
+                      (comp xf last))
+                (partial mapv second)
+                (partial re-seq rgx)))
+        (transduce + (line-seq rdr)))))
+
