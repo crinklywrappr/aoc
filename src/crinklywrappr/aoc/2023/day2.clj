c369e241add8225016d38b63566671bd4375e079
Daniel Fitzpatrick <crinklywrappr@pm.me>
2023-12-02 00:27:59 -0600

    rewrite 2023 day 2 with the correct intuition

--- src/crinklywrappr/aoc/2023/day2.clj
+++ src/crinklywrappr/aoc/2023/day2.clj
@@ -4,25 +4,29 @@
 
 (def file (io/resource "2023/day2.txt"))
 
+(def regex #"\d+|red|green|blue")
+
+(defn parse-element [x]
+  (if (re-matches #"\d+" x)
+    (parse-long x) (keyword x)))
+
+(defn maximum-counts [[game & reveals]]
+  [game
+   (reduce
+    (fn [maximums [count color]]
+      (if (> count (get maximums color))
+        (assoc maximums color count)
+        maximums))
+    {:red 0 :green 0 :blue 0}
+    (partition 2 reveals))])
+
 (defn parse-line [line]
-  (let [[game record] (sg/split line #":")
-        game (parse-long (re-find #"\d+" game))
-        reveals (sg/split record #";")
-        parse (fn [[count color]] [(parse-long count) (keyword color)])
-        parsed-reveals (mapcat (comp (partial map (comp parse rest))
-                                  (partial re-seq #"(\d+) ([^,]+)"))
-                               reveals)]
-    [game parsed-reveals]))
+  (->> (re-seq regex line)
+       (mapv parse-element)
+       maximum-counts))
 
-;; Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes.
-(defn below-threshold? [[count color]]
-  (cond
-    (= color :red) (<= count 12)
-    (= color :green) (<= count 13)
-    (= color :blue) (<= count 14)))
-
-(defn possible-game? [[game reveals]]
-  (every? below-threshold? reveals))
+(defn possible-game? [[_ {:keys [red green blue]}]]
+  (and (<= red 12) (<= green 13) (<= blue 14)))
 
 (defn part1 []
   (with-open [rdr (io/reader file)]
@@ -31,17 +35,9 @@
            (map first))
         (transduce + (line-seq rdr)))))
 
-(defn power [[_ reveals]]
-  (->> reveals
-       (reduce
-        (fn [maximums [count color]]
-          (if (> count (get maximums color))
-            (assoc maximums color count)
-            maximums))
-        {:red 0 :green 0 :blue 0})
-       vals (apply *)))
-
 (defn part2 []
-  (with-open [rdr (io/reader file)]
-    (-> (map (comp power parse-line))
-        (transduce + (line-seq rdr)))))
+  (letfn [(power [[_ reveals]]
+            (apply * (vals reveals)))]
+    (with-open [rdr (io/reader file)]
+      (-> (map (comp power parse-line))
+          (transduce + (line-seq rdr))))))
