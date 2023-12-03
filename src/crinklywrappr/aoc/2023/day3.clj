b2b8b045641dc08bb6eebc72077381152a175b88
Daniel Fitzpatrick <crinklywrappr@pm.me>
2023-12-03 17:58:03 -0600

    memoize hot functions that are called a lot

--- src/crinklywrappr/aoc/2023/day3.clj
+++ src/crinklywrappr/aoc/2023/day3.clj
@@ -10,16 +10,17 @@
 (defn symbols-adjacent? [symbols [num-idx number]]
   (some symbols (range (dec num-idx) (+ num-idx (count number) 1))))
 
-(defn part-numbers [[sum above line] below]
-  (let [symbols (->> (merge (util/re-pos syms above)
-                            (util/re-pos syms line)
-                            (util/re-pos syms below))
-                     keys set)
-        numbers (util/re-pos nums line)]
-    [(-> (comp (filter (partial symbols-adjacent? symbols))
-            (map (comp parse-long second)))
-         (transduce + numbers) (+ sum))
-     line below]))
+(let [re-pos (memoize util/re-pos)]
+  (defn part-numbers [[sum above line] below]
+    (let [symbols (->> (merge (re-pos syms above)
+                              (re-pos syms line)
+                              (re-pos syms below))
+                       keys set)
+          numbers (util/re-pos nums line)]
+      [(-> (comp (filter (partial symbols-adjacent? symbols))
+              (map (comp parse-long second)))
+           (transduce + numbers) (+ sum))
+       line below])))
 
 (defn part1 []
   (with-open [rdr (io/reader file)]
@@ -44,15 +45,16 @@
      (assoc a [idx (+ idx (dec (count number)))] [number]))
    {} (util/re-pos nums line)))
 
-(defn gear-ratios [[sum above line] below]
-  (let [numbers (merge-with concat
-                            (gather-numbers above)
-                            (gather-numbers line)
-                            (gather-numbers below))
-        symbols (keys (util/re-pos #"\*" line))]
-    [(-> (map (partial gear-ratio numbers))
-         (transduce + symbols) (+ sum))
-     line below]))
+(let [gather-numbers (memoize gather-numbers)]
+  (defn gear-ratios [[sum above line] below]
+    (let [numbers (merge-with concat
+                              (gather-numbers above)
+                              (gather-numbers line)
+                              (gather-numbers below))
+          symbols (keys (util/re-pos #"\*" line))]
+      [(-> (map (partial gear-ratio numbers))
+           (transduce + symbols) (+ sum))
+       line below])))
 
 (defn part2 []
   (with-open [rdr (io/reader file)]
