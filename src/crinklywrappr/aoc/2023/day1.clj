38c583dc61408f52a4be7b64dd02047a43a56673
Daniel Fitzpatrick <crinklywrappr@pm.me>
2023-12-02 22:20:43 -0600

    localize vars

--- src/crinklywrappr/aoc/2023/day1.clj
+++ src/crinklywrappr/aoc/2023/day1.clj
@@ -11,16 +11,15 @@
                 (partial re-seq #"\d")))
         (transduce + (line-seq rdr)))))
 
-(def rgx #"(?=(one|two|three|four|five|six|seven|eight|nine|\d))")
-(def xf {"one" 1 "two" 2 "three" 3 "four" 4 "five" 5 "six" 6 "seven" 7 "eight" 8 "nine" 9
-         "1" 1 "2" 2 "3" 3 "4" 4 "5" 5 "6" 6 "7" 7 "8" 8 "9" 9})
-
 (defn part2 []
-  (with-open [rdr (io/reader file)]
-    (-> (map (comp (partial apply +)
-                (juxt (comp (partial * 10) xf first)
-                      (comp xf last))
-                (partial mapv second)
-                (partial re-seq rgx)))
-        (transduce + (line-seq rdr)))))
+  (let [rgx #"(?=(one|two|three|four|five|six|seven|eight|nine|\d))"
+        xf {"one" 1 "two" 2 "three" 3 "four" 4 "five" 5 "six" 6 "seven" 7 "eight" 8 "nine" 9
+            "1" 1 "2" 2 "3" 3 "4" 4 "5" 5 "6" 6 "7" 7 "8" 8 "9" 9}]
+    (with-open [rdr (io/reader file)]
+      (-> (map (comp (partial apply +)
+                  (juxt (comp (partial * 10) xf first)
+                        (comp xf last))
+                  (partial mapv second)
+                  (partial re-seq rgx)))
+          (transduce + (line-seq rdr))))))
 
