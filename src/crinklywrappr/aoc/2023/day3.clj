a9892289dd5f28a26431d5f36b6e90e1d4cd4e01
Daniel Fitzpatrick <crinklywrappr@pm.me>
2023-12-03 16:32:20 -0600

    add 2023 day 3 solutions

---
+++ src/crinklywrappr/aoc/2023/day3.clj
@@ -0,0 +1,60 @@
+(ns crinklywrappr.aoc.2023.day3
+  (:require [clojure.java.io :as io]
+            [crinklywrappr.aoc.util :as util]))
+
+(def file (io/resource "2023/day3.txt"))
+
+(def syms #"[^\d.]")
+(def nums #"\d+")
+
+(defn symbols-adjacent? [symbols [num-idx number]]
+  (some symbols (range (dec num-idx) (+ num-idx (count number) 1))))
+
+(defn part-numbers [[sum above line] below]
+  (let [symbols (->> (merge (util/re-pos syms above)
+                            (util/re-pos syms line)
+                            (util/re-pos syms below))
+                     keys set)
+        numbers (util/re-pos nums line)]
+    [(-> (comp (filter (partial symbols-adjacent? symbols))
+            (map (comp parse-long second)))
+         (transduce + numbers) (+ sum))
+     line below]))
+
+(defn part1 []
+  (with-open [rdr (io/reader file)]
+    (let [wrap (apply str (repeat 140 \.))
+          lines (util/wrap-line-seq rdr wrap)]
+      (first (reduce part-numbers [0 wrap (first lines)] (rest lines))))))
+
+(defn number-adjacent? [symbol [[minimum maximum] _]]
+  (or (<= minimum (dec symbol) maximum)
+      (== symbol minimum)
+      (<= minimum (inc symbol) maximum)))
+
+(defn gear-ratio [numbers symbol]
+  (let [adjacent (apply concat (vals (filterv (partial number-adjacent? symbol) numbers)))]
+    (if (== (count adjacent) 2)
+      (apply * (mapv parse-long adjacent)) 0)))
+
+(defn gather-numbers [line]
+  (reduce
+   (fn [a [idx number]]
+     (assoc a [idx (+ idx (dec (count number)))] [number]))
+   {} (util/re-pos nums line)))
+
+(defn gear-ratios [[sum above line] below]
+  (let [numbers (merge-with concat
+                            (gather-numbers above)
+                            (gather-numbers line)
+                            (gather-numbers below))
+        symbols (keys (util/re-pos #"\*" line))]
+    [(-> (map (partial gear-ratio numbers))
+         (transduce + symbols) (+ sum))
+     line below]))
+
+(defn part2 []
+  (with-open [rdr (io/reader file)]
+    (let [wrap (apply str (repeat 140 \.))
+          lines (util/wrap-line-seq rdr wrap)]
+      (first (reduce gear-ratios [0 wrap (first lines)] (rest lines))))))
--- src/crinklywrappr/aoc/util.clj
+++ src/crinklywrappr/aoc/util.clj
@@ -2,9 +2,26 @@
   (:import [java.io BufferedReader]))
 
 (defn char-seq
-  "ascii-only support"
   [^BufferedReader rdr]
   (let [byte (.read rdr)]
     (when (pos? byte)
-      (cons (char byte)
-            (lazy-seq (char-seq rdr))))))
+      (cons (char byte) (lazy-seq (char-seq rdr))))))
+
+(defn wrap-line-seq
+  "prepends and/or appends a line to the file when reading"
+  ([^BufferedReader rdr before after]
+   (cons before (lazy-seq (wrap-line-seq rdr after))))
+  ([^BufferedReader rdr after]
+   (if-let [line (.readLine rdr)]
+     (cons line (lazy-seq (wrap-line-seq rdr after)))
+     [after])))
+
+;; Stolen from Vincent Ho, who stole it from SO
+(defn re-pos [re s]
+  (if (nil? re)
+    {}
+    (loop [m (re-matcher re s)
+           res {}]
+      (if (.find m)
+        (recur m (assoc res (.start m) (.group m)))
+        res))))
