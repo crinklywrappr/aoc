a953587d4bd8bdb399a32d02d7b26ebcd15365c6
Daniel Fitzpatrick <crinklywrappr@pm.me>
2024-12-07 17:28:38 -0600

    remove interleave-all, optimize day 7

--- src/crinklywrappr/aoc/2024/day07.clj
+++ src/crinklywrappr/aoc/2024/day07.clj
@@ -1,6 +1,6 @@
 (ns crinklywrappr.aoc.2024.day07
-  (:require [clojure.java.io :as io]
-            [crinklywrappr.aoc.util :as util]
+  (:require [clojure.math :as math]
+            [clojure.java.io :as io]
             [clojure.math.combinatorics :as combo]))
 
 (def file (io/resource "2024/day07.txt"))
@@ -9,24 +9,20 @@
   (->> (re-seq #"\d+" line)
        (mapv parse-long)))
 
-(defn apply-operator [solution total [operator input]]
-  (let [total' (operator total input)]
-    (if (> total' solution)
-      (reduced total')
-      total')))
+(defn solve-equation [solution inputs operations]
+  (loop [solution' (first inputs) [x & xs] (rest inputs) [op & ops] operations]
+    (if (or (nil? x) (> solution' solution))
+      solution'
+      (recur (op solution' x) xs ops))))
 
-(defn solve [solution total equation]
-  (let [solution' (reduce (partial apply-operator solution)
-                          (first equation)
-                          (partition 2 (rest equation)))]
-    (if (== solution' solution)
-      (reduced (+ total solution))
-      total)))
+(defn solvable? [solution inputs total operation]
+  (if (== solution (solve-equation solution inputs operation))
+    (reduced (+ total solution))
+    total))
 
 (defn valid-inputs [operators total [solution & inputs]]
-  (let [operations (combo/selections operators (dec (count inputs)))]
-    (->> (map util/interleave-all (repeat (count operations) inputs) operations)
-         (reduce (partial solve solution) total))))
+  (->> (combo/selections operators (dec (count inputs)))
+       (reduce (partial solvable? solution inputs) total)))
 
 (defn part1 []
   (with-open [rdr (io/reader file)]
@@ -35,7 +31,7 @@
                    0 (line-seq rdr)))))
 
 (defn concat-numbers [a b]
-  (parse-long (str a b)))
+  (+ b (* a (long (math/pow 10 (inc (math/floor (math/log10 b))))))))
 
 (defn part2 []
   (with-open [rdr (io/reader file)]
