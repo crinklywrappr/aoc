3af0c2376b7e76da011b69a32b22f8a439cb47cf
Daniel Fitzpatrick <crinklywrappr@pm.me>
2024-12-14 02:58:17 -0600

    2022 day 7 simpler (my original solution, mostly)

--- src/crinklywrappr/aoc/2022/day07.clj
+++ src/crinklywrappr/aoc/2022/day07.clj
@@ -1,79 +1,36 @@
 (ns crinklywrappr.aoc.2022.day07
   (:require [clojure.java.io :as io]
-            [clojure.string :as sg]
-            [clojure.zip :as z]))
+            [clojure.string :as sg]))
 
 (def file (io/resource "2022/day07.txt"))
 
-(defn dir? [node]
-  (contains? (meta node) :children))
-
-(defn filesystem []
-  (z/zipper
-   dir? (comp seq :children meta)
-   (fn make-node [node children]
-     (with-meta node {:children children}))
-   (with-meta {:filename "/"} {:children []})))
-
-(defn make-dir-node [filename]
-  (with-meta {:filename filename} {:children []}))
-
-(defn make-file-node [filename size]
-  (with-meta {:filename filename} {:size size}))
-
-(defn root [filesystem]
-  (if-let [parent (z/up filesystem)]
-    (recur parent)
-    filesystem))
-
-(defn cd [filesystem file]
-  (loop [filesystem (z/down filesystem)]
-    (let [{:keys [filename]} (z/node filesystem)]
-      (if (= filename file)
-        filesystem
-        (recur (z/right filesystem))))))
-
-(defn populate-filesystem [filesystem line]
-  (cond
-    (= line "$ ls") filesystem
-    (= line "$ cd /") (root filesystem)
-    (= line "$ cd ..") (z/up filesystem)
-    (sg/starts-with? line "$ cd") (cd filesystem (.substring line 5))
-    (sg/starts-with? line "dir") (->> (.substring line 4)
-                                      make-dir-node
-                                      (z/insert-child filesystem))
-    :else (let [[size filename] (sg/split line #" ")]
-            (->> (parse-long size)
-                 (make-file-node filename)
-                 (z/insert-child filesystem)))))
-
-(defn update-totals [directories filesystem size]
+(defn add-filesize [filesystem path size]
   (first
    (reduce
-    (fn [[directories' path] {:keys [filename]}]
-      (let [path' (conj path filename)]
-        [(update directories' path' (fnil + 0) size) path']))
-    [directories []] (reverse (z/path filesystem)))))
+    (fn [[fs p] d]
+      (let [p' (conj p d)]
+        [(update fs p' (fnil + 0) size) p']))
+    [filesystem []] path)))
 
-(defn df [filesystem]
-  (loop [directories {} filesystem (root filesystem)]
-    (if (z/end? filesystem)
-      directories
-      (let [node (z/node filesystem)]
-        (if (dir? node)
-          (recur directories (z/next filesystem))
-          (recur (update-totals directories filesystem (:size (meta node)))
-                 (z/next filesystem)))))))
+(defn populate-filesystem [[filesystem path] line]
+  (cond
+    (= line "$ ls") [filesystem path]
+    (= line "$ cd /") [filesystem ["/"]]
+    (= line "$ cd ..") [filesystem (pop path)]
+    (sg/starts-with? line "$ cd") [filesystem (conj path (.substring line 5))]
+    (sg/starts-with? line "dir") [(assoc filesystem (conj path (.substring line 4)) 0) path]
+    :else (let [[size _] (sg/split line #" ")]
+            [(add-filesize filesystem path (parse-long size)) path])))
 
 (defn part1 []
   (with-open [rdr (io/reader file)]
     (reduce-kv
      (fn [a _ v] (if (< v 100000) (+ a v) a))
-     0 (df (reduce populate-filesystem (filesystem) (line-seq rdr))))))
+     0 (first (reduce populate-filesystem [{} ["/"]] (line-seq rdr))))))
 
 (defn part2 []
   (with-open [rdr (io/reader file)]
-    (let [size-map (df (reduce populate-filesystem (filesystem) (line-seq rdr)))
+    (let [size-map (first (reduce populate-filesystem [{} ["/"]] (line-seq rdr)))
           target (- 30000000 (- 70000000 (get size-map ["/"])))]
       (reduce-kv
        (fn [a _ v] (if (and (>= v target) (< v a)) v a))
