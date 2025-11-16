1a1c7a5026e77541a9c2d4244460b42e4095f3b1
Daniel Fitzpatrick <crinklywrappr@pm.me>
2025-11-16 13:49:22 -0600

    2018 day 1

---
+++ src/crinklywrappr/aoc/2018/day01.clj
@@ -0,0 +1,20 @@
+(ns crinklywrappr.aoc.2018.day01
+  (:require [clojure.java.io :as io]))
+
+(def file (io/resource "2018/day01.txt"))
+
+(defn part1 []
+  (with-open [rdr (io/reader file)]
+    (transduce (map parse-long) + (line-seq rdr))))
+
+(defn part2 []
+  (with-open [rdr (io/reader file)]
+    (transduce
+     (map parse-long)
+     (completing
+      (fn [[freq seen] change]
+        (let [new-freq (+ freq change)]
+          (if (seen new-freq)
+            (reduced new-freq)
+            [new-freq (conj seen new-freq)]))))
+     [0 #{0}] (cycle (line-seq rdr)))))
